e20073e8351415a6c9467b541ea01d95ffae4146
Denis Belocerkovec <belocerkovecden@gmail.com>
2017-08-09 08:15:00 +0300

    Update login.php

--- graduation_projects/burgers/php/login.php
+++ graduation_projects/burgers/php/login.php
@@ -78,7 +78,6 @@ function clean($value = '')
     return $value;
 }
 
-
 /* Отправляю письмо
 ============================================================*/
 function go_mail($validEmail, $quantity_arr, $id_order, $street, $home, $housing, $appt)
