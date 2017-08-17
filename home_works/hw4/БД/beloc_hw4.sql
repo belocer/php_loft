23718fafab27416e394874cf346bf274789fae4c
belocer <belocerkovecden@gmail.com>
2017-08-17 13:20:18 +0300

    Вроде бы всё поправил, посмотри пожалуйста

--- home_works/hw4/БД/beloc_hw4.sql
+++ home_works/hw4/БД/beloc_hw4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 15 2017 г., 11:29
+-- Время создания: Авг 17 2017 г., 13:19
 -- Версия сервера: 5.7.11
 -- Версия PHP: 7.0.4
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `age` int(10) DEFAULT NULL,
   `description` text,
   `photo` text
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
@@ -42,9 +42,10 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `login`, `password`, `name`, `age`, `description`, `photo`) VALUES
 (17, 'лололо', 'hwUstR.N6PdQU', 'лололо', 4, 'лолололололол', './photos/5992aef3d47491.jpg'),
-(20, 'qqqqqq', 'hwr4MWaHehrH6', 'qqqqqq', 99, 'qqqqqq', './photos/5992acf2c65442017-07-31_18-08-14.png'),
 (21, 'wwwwww', 'hwIvVJSs7WXL2', 'wwwwww', 222, 'wwwwwwwwwwwww', './photos/59929b7dcc2c81.jpg'),
-(22, 'ботбот', 'hw6BudXmwEWjA', 'бот', 0, 'ботботботботботботботбот', './photos/5992b07b4290ascreenshot_2.jpg');
+(22, 'ботбот', 'hw6BudXmwEWjA', 'бот', 0, 'ботботботботботботботбот', ''),
+(23, 'Кобряныч', 'hwm26c5teUmio', 'Денис', 33, 'Слон Велосипедовичxxx', './photos/59956acb31527.jpg'),
+(24, 'ssssss', 'hwBASvsh9USOs', 'ssssss', 65, 'ssssss', './photos/59956b6377bc5oboi7.com-19075.jpg');
 
 --
 -- Индексы сохранённых таблиц
@@ -65,7 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
